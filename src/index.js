6db26d008a977193e993078e189817abbe4073cb
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-09 12:28:07 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// importoing necessary modules
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
